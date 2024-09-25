de5dc6f544541fc2af905cd152a521e2948decc9
memset0 <memset0@outlook.com>
2024-09-25 15:21:55 +0800

    ci

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory('/itp-qualification/'),
+  routes: [
+    // 您的路由配置
+  ],
+});
+
+export default router;
